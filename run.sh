9bb5786099ee7b218e810b553f6fb2318941572c
shanevcantwell <153727980+shanevcantwell@users.noreply.github.com>
2025-10-25 07:09:01 -0600

    Housecleaning

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
-echo "Starting Ollama Shim server in $SCRIPT_DIR"
-cd "$SCRIPT_DIR" || exit 1 # Navigate to the script's directory or exit if failed.
-
-python3 -m uvicorn ollama_shim:app --host 0.0.0.0 --port 11434
